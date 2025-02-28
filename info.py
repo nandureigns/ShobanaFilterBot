4379c740f69e48f151d901a1ec7e0034df5b5bb9
MMW BOTZ <155316405+mmwbotzmain@users.noreply.github.com>
2025-02-28 14:41:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -37,10 +37,10 @@ AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://dboss:dboss1@cluster0.j3yv1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "dboss")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'mn_files)
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'mn_files')
 
 # Others
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001930856812)
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001930856812')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'KnMoviez')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', 'False')), False)
 IMDB = is_enabled((environ.get('IMDB', 'True')), True)
